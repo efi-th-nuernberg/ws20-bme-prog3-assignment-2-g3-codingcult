e7260b208edf7877343d6ac24a712ccb2e0e951e
repl.it user <replituser@example.com>
2020-10-26 16:09:13 +0000

    erster ansatz

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer; 
+
 class TextFormatter {
 
   private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
@@ -7,6 +9,7 @@ class TextFormatter {
           "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et " +
           "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
           "Lorem ipsum dolor sit amet.";
+  String temp = " ";
 
   public static void main(String[] args) {
     TextFormatter formatter = new TextFormatter(30);
@@ -15,12 +18,20 @@ class TextFormatter {
 
   // Konstruktor
   public TextFormatter(int maxLineLength) {
-    // ...
+  
+    for(int i = 0; i < (text.length()/maxLineLength); i++){
+
+//line 1
+     if(i == 0) {
+        temp = temp + (text.substring(i, maxLineLength) + "\n");
+      } else {
+       temp = temp + (text.substring(i*maxLineLength, i*maxLineLength+maxLineLength) + "\n");
+     }
+    }
   }
 
   // Ausgabe
   public void print(String aText) {
-    System.out.println("Hier sollte der Text mit passendem Umbruch erscheinen.");
+    System.out.println(temp);
   }
-
 }
