f9d8b7346f6fd8e60e8e9b7dcd8b28d6735846ee
repl.it user <replituser@example.com>
2020-10-29 08:04:37 +0000

    zweiter ansatz

--- TextFormatter.java
+++ TextFormatter.java
@@ -9,7 +9,8 @@ class TextFormatter {
           "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et " +
           "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
           "Lorem ipsum dolor sit amet.";
-  String temp = " ";
+
+  private String finalText = " ";
 
   public static void main(String[] args) {
     TextFormatter formatter = new TextFormatter(30);
@@ -18,20 +19,21 @@ class TextFormatter {
 
   // Konstruktor
   public TextFormatter(int maxLineLength) {
-  
-    for(int i = 0; i < (text.length()/maxLineLength); i++){
-
-//line 1
-     if(i == 0) {
-        temp = temp + (text.substring(i, maxLineLength) + "\n");
-      } else {
-       temp = temp + (text.substring(i*maxLineLength, i*maxLineLength+maxLineLength) + "\n");
-     }
+    StringTokenizer st = new StringTokenizer(text);
+    String temp = " ";
+    while (st.hasMoreTokens()) {
+      // keeps going as long as there are words
+      temp = temp + st.nextToken() + " ";
+      if (temp.length() > maxLineLength) {
+        finalText = finalText + temp + "\n";
+        temp = " ";
+      }
     }
+    finalText = finalText + temp;
   }
 
   // Ausgabe
   public void print(String aText) {
-    System.out.println(temp);
+    System.out.println(finalText);
   }
 }
