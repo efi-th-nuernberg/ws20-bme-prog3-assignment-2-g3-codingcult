751448c739f8ae190d9bf9706f02b1bbcae63d76
repl.it user <replituser@example.com>
2020-11-03 08:40:50 +0000

    vierter ansatz

--- TextFormatter.java
+++ TextFormatter.java
@@ -30,12 +30,62 @@ class TextFormatter {
     while (separateEachWord.hasMoreTokens()) {
       String word = separateEachWord.nextToken();
       if (temp.length() >= (maxLineLength - word.length())) {
+        // here we add method to align temp
+        temp = format_left_aligned(temp);
+        // temp = format_right_aligned(temp);
+        // temp = format_block(temp);
+        
         finalText = finalText + temp + "\n";
         temp = "";
+      } else {
+        temp = temp + word + " ";
       }
-      temp = temp + word + " ";
     }
+    // here we add method to align the last word/sentecne of temp
+    temp = format_left_aligned(temp);
+    // temp = format_right_aligned(temp);
+    // temp = format_block(temp);
+
     finalText = finalText + temp;
     System.out.println(finalText);
+  }
+
+  // default
+  public String format_left_aligned(String line) {
+    String temp = line;
+    for(int i = 0; i < (maxLineLength - line.length()); i++) {
+      temp = temp + " ";
+    }
+    return (temp);
+  }
+
+  public String format_right_aligned(String line) {
+    String temp = line;
+    for(int i = 0; i < (maxLineLength - line.length()); i++) {
+      temp = " " + temp;
+    }
+    return (temp);
+  }
+
+  // does not work yet
+  public String format_block(String line) {
+    String temp = line;
+    // how many space chars we need to add
+    for(int i = 0; i < (maxLineLength - temp.length()); i++) {
+      // searching for places in the string where we can add a space
+      for(int j = 0; j < temp.length()-1; j++) {
+        if(temp.charAt(j) == ' ') {
+          addChar(temp, '!', j);
+        }
+      }
+    }
+    return (temp);
+  }
+
+  //https://www.baeldung.com/java-add-character-to-string
+  public String addChar(String str, char ch, int position) {
+    StringBuilder sb = new StringBuilder(str);
+    sb.insert(position, ch);
+    return sb.toString();
   }
 }
