c4ecd0091fd2fc89d1f11468fb4a76c9d80ed59b
repl.it user <replituser@example.com>
2020-11-04 18:56:00 +0000

    7 ansatz

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,14 +1,14 @@
-import java.util.StringTokenizer; 
+import java.util.StringTokenizer;
 
 class TextFormatter {
 
-  private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
-          "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
-          "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
-          "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
-          "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et " +
-          "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
-          "Lorem ipsum dolor sit amet.";
+  private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
+      + "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et "
+      + "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem "
+      + "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod "
+      + "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et "
+      + "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est "
+      + "Lorem ipsum dolor sit amet.";
 
   private int maxLineLength;
   private String finalText = "";
@@ -18,12 +18,12 @@ class TextFormatter {
     formatter.print(text);
   }
 
-  // Konstruktor
+  // constructor
   public TextFormatter(int maxLineLength) {
     this.maxLineLength = maxLineLength;
   }
 
-  // Ausgabe
+  // outlay
   public void print(String aText) {
     StringTokenizer separateEachWord = new StringTokenizer(text);
     String temp = "";
@@ -31,20 +31,24 @@ class TextFormatter {
       String word = separateEachWord.nextToken();
       if (temp.length() >= (maxLineLength - word.length())) {
         // here we add method to align temp
-        temp = format_left_aligned(temp);
+        // temp = format_left_aligned(temp);
         // temp = format_right_aligned(temp);
-        // temp = format_block(temp);
-        
+        temp = format_block(temp);
+
         finalText = finalText + temp + "\n";
-        temp = "";
+        temp = word;
       } else {
-        temp = temp + word + " ";
+        if (temp.isEmpty()) {
+          temp = word;
+        } else {
+          temp = temp + " " + word;
+        }
       }
     }
     // here we add method to align the last word/sentecne of temp
-    temp = format_left_aligned(temp);
+    // temp = format_left_aligned(temp);
     // temp = format_right_aligned(temp);
-    // temp = format_block(temp);
+    temp = format_block(temp);
 
     finalText = finalText + temp;
     System.out.println(finalText);
@@ -53,7 +57,7 @@ class TextFormatter {
   // default
   public String format_left_aligned(String line) {
     String temp = line;
-    for(int i = 0; i < (maxLineLength - line.length()); i++) {
+    for (int i = 0; i < (maxLineLength - line.length()); i++) {
       temp = temp + " ";
     }
     return (temp);
@@ -61,31 +65,44 @@ class TextFormatter {
 
   public String format_right_aligned(String line) {
     String temp = line;
-    for(int i = 0; i < (maxLineLength - line.length()); i++) {
+    for (int i = 0; i < (maxLineLength - line.length()); i++) {
       temp = " " + temp;
     }
     return (temp);
   }
 
-  // does not work yet
+
   public String format_block(String line) {
     String temp = line;
+    int freeSpaces = maxLineLength - line.length();
+    int i = 0;
     // how many space chars we need to add
-    for(int i = 0; i < (maxLineLength - temp.length()); i++) {
-      // searching for places in the string where we can add a space
-      for(int j = 0; j < temp.length()-1; j++) {
-        if(temp.charAt(j) == ' ') {
-          addChar(temp, '!', j);
-        }
+    while(freeSpaces != 0 && returnAvailableSpaces(line) > 1) {
+      i++;
+      if (temp.charAt(i) == ' ') {
+        temp = addChar(temp, ' ', i);
+        freeSpaces--;
+        i++;
+      }
+      if(i >= temp.length() - 1) {
+          i = 0;
       }
     }
     return (temp);
   }
 
-  //https://www.baeldung.com/java-add-character-to-string
+  public int returnAvailableSpaces(String line) {
+    int availableSpaces = 0;
+    for (int i = 0; i < line.length() - 1; i++) {
+      if (line.charAt(i) == ' ') {
+        availableSpaces++;
+      }
+    }
+    return (availableSpaces);
+  }
+
+  // https://www.baeldung.com/java-add-character-to-string
   public String addChar(String str, char ch, int position) {
-    StringBuilder sb = new StringBuilder(str);
-    sb.insert(position, ch);
-    return sb.toString();
+    return str.substring(0, position) + ch + str.substring(position);
   }
 }
